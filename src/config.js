c48c1c8bc96bae282894ad2e7b9f1aa42e23508a
renoeno <reno.less@gmail.com>
2022-07-02 12:05:47 -0300

    First commit

---
+++ src/config.js
@@ -0,0 +1,31 @@
+function registerConfig({ current, buildPath }) {
+  return {
+    source: [current.source],
+    platforms: {
+      "web/css": {
+        transformGroup: "css",
+        buildPath: buildPath.css,
+        files: [
+          {
+            destination: `${current.filename}.css`,
+            format: "css/variables",
+          },
+        ],
+      },
+      "web/scss": {
+        transformGroup: "scss",
+        buildPath: buildPath.scss,
+        files: [
+          {
+            destination: `${current.filename}.scss`,
+            format: "scss/variables",
+          },
+        ],
+      },
+    },
+  };
+}
+
+module.exports = {
+  registerConfig,
+};
---
+++ src/index.js
@@ -0,0 +1,12 @@
+const path = require("path");
+const { getBrands } = require("./brand");
+const { buildTokens } = require("./build");
+
+getBrands().map(async function (current) {
+  const buildPath = {
+    css: path.join("dist", "css", current.dest, path.sep),
+    scss: path.join("dist", "scss", current.dest, path.sep),
+  };
+
+  await buildTokens({ current, buildPath });
+});
